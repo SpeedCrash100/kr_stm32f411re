0b1cc0ae6f1464da3f76e94c787f78d5d79a5d19
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-10-13 09:14:51 +0300

    Use CC_IRQn instead update

--- Src/pwm.c
+++ Src/pwm.c
@@ -1,145 +1,135 @@
 #include "pwm.h"
-#include "stm32f4xx_hal.h"
+
 #include <math.h>
+#include <stdlib.h>
+
+#include "stm32f4xx_hal.h"
 
 #define BUFFER_SIZE 102400
 
 struct PWM {
-	TIM_HandleTypeDef tim1;
-	TIM_OC_InitTypeDef chanConfig;
+  TIM_HandleTypeDef tim1;
+  TIM_OC_InitTypeDef chanConfig;
 
-	uint8_t width;
-	uint32_t freq;
+  uint8_t width;
+  uint32_t freq;
 
-	Boolean valueCaptured;
-	uint8_t buffer[BUFFER_SIZE];
+  Boolean valueCaptured;
+  uint8_t buffer[BUFFER_SIZE];
 
-	//TMP
-	uint32_t bufferPos;
-	uint32_t bufferWritePos;
+  // TMP
+  uint32_t bufferPos;
+  uint32_t bufferWritePos;
 };
 
 Boolean g_PWM_initialized = FALSE;
 PWM g_PWM = {0};
 
-PWM* PWM_Init()
-{
-	if(g_PWM_initialized)
-		return &g_PWM;
+PWM* PWM_Init() {
+  if (g_PWM_initialized) return &g_PWM;
 
-	TIM_HandleTypeDef* tim1 = &g_PWM.tim1;
-	TIM_OC_InitTypeDef* chanConfig = &g_PWM.chanConfig;
+  TIM_HandleTypeDef* tim1 = &g_PWM.tim1;
+  TIM_OC_InitTypeDef* chanConfig = &g_PWM.chanConfig;
 
-	tim1->Instance = TIM1;
-	tim1->Init.Prescaler = 38;
-	tim1->Init.CounterMode = TIM_COUNTERMODE_UP;
-	tim1->Init.Period = 256;
-	tim1->Init.ClockDivision = TIM_CLOCKPRESCALER_DIV1;
-	tim1->Init.RepetitionCounter = 0;
-	if(HAL_TIM_Base_Init(tim1) != HAL_OK)
-		return NULL;
+  tim1->Instance = TIM1;
+  tim1->Init.Prescaler = 38;
+  tim1->Init.CounterMode = TIM_COUNTERMODE_UP;
+  tim1->Init.Period = 256;
+  tim1->Init.ClockDivision = TIM_CLOCKPRESCALER_DIV1;
+  tim1->Init.RepetitionCounter = 0;
+  if (HAL_TIM_Base_Init(tim1) != HAL_OK) return NULL;
 
-	if(HAL_TIM_PWM_Init(tim1) != HAL_OK)
-		return NULL;
+  if (HAL_TIM_PWM_Init(tim1) != HAL_OK) return NULL;
 
-	HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 0, 0);
-	HAL_NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
-	__HAL_TIM_ENABLE_IT(tim1, TIM_IT_UPDATE);
+  HAL_NVIC_SetPriority(TIM1_CC_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
 
-	chanConfig->OCMode = TIM_OCMODE_PWM1;
-	chanConfig->OCPolarity = TIM_OCPOLARITY_HIGH;
-	chanConfig->Pulse = 0;
-	if(HAL_TIM_PWM_ConfigChannel(tim1, chanConfig, TIM_CHANNEL_1) != HAL_OK)
-		return NULL;
+  chanConfig->OCMode = TIM_OCMODE_PWM1;
+  chanConfig->OCPolarity = TIM_OCPOLARITY_HIGH;
+  chanConfig->Pulse = 0;
+  if (HAL_TIM_PWM_ConfigChannel(tim1, chanConfig, TIM_CHANNEL_1) != HAL_OK)
+    return NULL;
 
-	g_PWM.freq = 1000;
-	g_PWM.width = 0;
+  g_PWM.freq = 1000;
+  g_PWM.width = 0;
 
-	//TODO! Queue
-	g_PWM.valueCaptured = TRUE;
-	g_PWM.bufferPos = 0;
-	g_PWM.bufferWritePos = 0;
+  // TODO! Queue
+  g_PWM.valueCaptured = TRUE;
+  g_PWM.bufferPos = 0;
+  g_PWM.bufferWritePos = 0;
 
+  g_PWM_initialized = TRUE;
 
-	g_PWM_initialized = TRUE;
-
-
-
-	return &g_PWM;
+  return &g_PWM;
 }
 
-int32_t PWM_SetFreq(PWM* hnd, int32_t freq)
-{
-	uint32_t prescal = 1;
-	uint32_t realFreq = SystemCoreClock / ( (256+1)*(prescal+1) );
-	uint32_t delta = abs(freq - realFreq);
-	while(prescal < 65535)
-	{
-		prescal++;
-		realFreq = SystemCoreClock / ( (256+1)*(prescal+1) );
-		uint32_t newDelta = abs(freq - realFreq);
-		if(newDelta < delta) {
-			delta = newDelta;
-		} else {
-			prescal--; // Revert
-			break;
-		}
-	}
+int32_t PWM_SetFreq(PWM* hnd, int32_t freq) {
+  uint32_t prescal = 1;
+  uint32_t realFreq = SystemCoreClock / ((256 + 1) * (prescal + 1));
+  uint32_t delta = abs(freq - (int)realFreq);
+  while (prescal < 65535) {
+    prescal++;
+    realFreq = SystemCoreClock / ((256 + 1) * (prescal + 1));
+    uint32_t newDelta = abs(freq - (int)realFreq);
+    if (newDelta < delta) {
+      delta = newDelta;
+    } else {
+      prescal--;  // Revert
+      break;
+    }
+  }
 
-	__HAL_TIM_SET_PRESCALER(&hnd->tim1, prescal);
-	return realFreq = SystemCoreClock / ( (256+1)*(prescal+1) );
+  __HAL_TIM_SET_PRESCALER(&hnd->tim1, prescal);
+  return realFreq = SystemCoreClock / ((256 + 1) * (prescal + 1));
 }
 
-void PWM_AddWidth(PWM* hnd, uint8_t width)
-{
+void PWM_AddWidth(PWM* hnd, uint8_t width) {
+  hnd->buffer[hnd->bufferWritePos] = width * 20 / 100;
+  hnd->bufferWritePos++;
+  hnd->bufferWritePos %= BUFFER_SIZE;
 
-	hnd->buffer[hnd->bufferWritePos] = width;
-	hnd->bufferWritePos++;
-	hnd->bufferWritePos %= BUFFER_SIZE;
-
-//	__HAL_TIM_SET_COMPARE(&hnd->tim1, TIM_CHANNEL_1, width);
+  //	__HAL_TIM_SET_COMPARE(&hnd->tim1, TIM_CHANNEL_1, width);
 }
 
-void PWM_Start(PWM* hnd)
-{
-	HAL_TIMEx_OCN_Start_IT(&hnd->tim1, TIM_CHANNEL_1);
+void PWM_Start(PWM* hnd) { HAL_TIMEx_OCN_Start_IT(&hnd->tim1, TIM_CHANNEL_1); }
+
+void PWM_Stop(PWM* hnd) { HAL_TIMEx_OCN_Stop_IT(&hnd->tim1, TIM_CHANNEL_1); }
+
+void PWM_GetBufferUsage(PWM* hnd, int32_t* usage) {
+  if (usage == NULL) return;
+
+  // TODO! Use queue size
+  if (hnd->bufferPos <= hnd->bufferWritePos) {
+    *usage = (hnd->bufferWritePos - hnd->bufferPos) * 100 / BUFFER_SIZE;
+  } else {
+    *usage = (BUFFER_SIZE - (hnd->bufferPos - hnd->bufferWritePos)) * 100 /
+             BUFFER_SIZE;
+  }
 }
 
-void PWM_Stop(PWM* hnd)
-{
-	HAL_TIMEx_OCN_Stop_IT(&hnd->tim1, TIM_CHANNEL_1);
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base) {
+  if (htim_base->Instance == TIM1) {
+    __HAL_RCC_TIM1_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    /**TIM1 GPIO Configuration
+    PA7     ------> TIM1_CH1N
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  }
 }
 
-void PWM_GetBufferUsage(PWM* hnd, int32_t* usage)
-{
+void TIM1_CC_IRQHandler() {
+  __HAL_TIM_CLEAR_IT(&g_PWM.tim1, TIM_IT_CC1);
 
+  __HAL_TIM_SET_COMPARE(&g_PWM.tim1, TIM_CHANNEL_1,
+                        g_PWM.buffer[g_PWM.bufferPos]);
+  g_PWM.bufferPos++;
+  g_PWM.bufferPos %= BUFFER_SIZE;
+  g_PWM.valueCaptured = TRUE;
 }
-
-void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
-{
-	if(htim_base->Instance == TIM1)
-	{
-		__HAL_RCC_TIM1_CLK_ENABLE();
-		__HAL_RCC_GPIOA_CLK_ENABLE();
-		GPIO_InitTypeDef GPIO_InitStruct = {0};
-		/**TIM1 GPIO Configuration
-		PA7     ------> TIM1_CH1N
-		*/
-		GPIO_InitStruct.Pin = GPIO_PIN_7;
-		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-		GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
-		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-	}
-}
-
-void TIM1_UP_TIM10_IRQHandler() {
-	__HAL_TIM_CLEAR_IT(&g_PWM.tim1, TIM_IT_UPDATE);
-
-	__HAL_TIM_SET_COMPARE(&g_PWM.tim1, TIM_CHANNEL_1, g_PWM.buffer[g_PWM.bufferPos]);
-	g_PWM.bufferPos++;
-	g_PWM.bufferPos %= BUFFER_SIZE;
-	g_PWM.valueCaptured = TRUE;
-}
-
