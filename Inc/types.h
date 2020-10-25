cb16d363c0e4c55518106f6441cc08e721d4ecb9
SpeedCrash100 <SpeedCrash100@yandex.ru>
2020-10-25 10:31:06 +0300

    Types docs

--- Inc/types.h
+++ Inc/types.h
@@ -3,24 +3,29 @@
 
 #include <stdint.h>
 
+/// \defgroup Types Вспомогательные определения
+/// @{
+
+/// Логический тип используемый в проекте
 typedef enum Boolean {
-	FALSE = 0,
-	TRUE
+  FALSE = 0,  /// Ложь
+  TRUE        /// Истина
 } Boolean;
 
-
+/// Рабочие состояния системы
 typedef enum MainStates {
-	Stopped,
-	Waiting,
-	Started,
-	Stopping
+  Stopped,  /// Генерация ШИМ остановлена
+  Waiting,  /// Ожидание модулирующих значений
+  Started,  /// Генерация ШИМ запущена
+  Stopping  /// Генерация ШИМ воспроизводит остатки буфера и останавливается
 } MainStates;
 
+/// Точка в пространстве дисплея
 typedef struct {
-	int32_t x;
-	int32_t y;
+  int32_t x;  /// Горизонтальная координата
+  int32_t y;  /// Вертекальная координата
 } Point;
 
-
+/// @}
 
 #endif /* TYPES_H_ */
